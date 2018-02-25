709414171baa5cb1b9370a04aaa569c437fee85f
Candy Barbaran <candybarbaran@Candys-MacBook-Pro.local>
2018-02-25 15:11:12 -0500

    Initial commit

---
+++ app/src/main/java/barbapplications/sft/user.kt
@@ -0,0 +1,12 @@
+package barbapplications.sft
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class user : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
